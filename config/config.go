752a0fd2c14cef6a13da199d2d6a6f6f8c05131c
faisalmashuri16 <faisal.mashuri.konsultan@work.bri.co.id>
2023-10-29 01:20:19 +0700

    change version

--- config/config.go
+++ config/config.go
@@ -1,7 +1,7 @@
 package config
 
 var (
-	Version       = "1.0.0"
+	Version       = "1.0.6"
 	WireCmd       = "github.com/google/wire/cmd/wire@latest"
 	BrixCmd       = "github.com/brix-go/brix"
 	FiberRepo     = "https://github.com/brix-go/fiber-boilerplate"
